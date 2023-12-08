af510ab8d0b5473536a53155a485edfe24ad8040
Ronak Malik <ronakmalik4242@gmail.com>
2023-12-08 14:11:27 -0500

    Fix AMM mech

--- mechanism.py
+++ mechanism.py
@@ -3,23 +3,35 @@ class AMM:
         self.players = players
 
     def buy_player(self, player, bid):
-        if bid >= self.players.loc[player]["Price"]:
+        if bid >= self.players.loc[player]:
             return True
 
         return False
     
     def sell_player(self, player, ask):
-        if ask <= self.players.loc[player]["Price"]:
+        if ask <= self.players.loc[player]:
             return True
         
         return False
     
 class CDA:
     def __init__(self):
-        pass
+        self.player_bids = {}
+        self.player_asks = {}
 
-    def buy_player(self, player, bid):
-        pass
+    def buy_player(self, agent, player, bid):
+        if player not in self.player_bids:
+            self.player_bids[player] = []
+        self.player_bids[player].append((agent, bid))
 
-    def sell_player(self, player, ask):
-        pass
+    def sell_player(self, agent, player, ask):
+        if player not in self.player_asks:
+            self.player_asks[player] = []
+        self.player_asks[player].append((agent, ask))
+
+    def resolve_trades(self):
+        '''
+        Returns list of tuples of the form (agent1, player1, ask, agent2, player2, bid),
+        where agent1 trades 
+        '''
+        
